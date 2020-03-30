98be9acfdaaed74eac23964193497b88df44654d
teiband <thomas.eiband@gmail.com>
2020-03-30 23:36:33 +0200

    add test cases for plotting

---
+++ tests/test_plot_collection.py
@@ -0,0 +1,42 @@
+import os
+#os.environ['PYTHONPATH'] = '$PYTHONPATH:/home/pi/workspace/homesens-devel/'
+os.chdir('../')
+from homesens.plot_collection import *
+import pickle
+from homesens.homesens import *
+import pytest
+
+
+@pytest.fixture()
+def db_entries():
+	with app.app_context():
+		db = get_db()
+		cur = db.execute('select datetime (timestamp,\'localtime\'), temperature, pressure, humidity from entries order by id desc')
+		entries = cur.fetchall()
+		return entries
+
+def test_sqlite_query():
+	with app.app_context():
+		db = get_db()
+		cur = db.execute('select datetime (timestamp,\'localtime\'), temperature, pressure, humidity from entries order by id desc')
+		global entries
+		entries = cur.fetchall()
+		
+	try:
+		os.system('rm ' + plot_collection.TMP_FILENAME_PREFIX + '*') # delete old files
+	except:
+		print "no files to delete"
+	
+	
+def test_plot_collection(db_entries):
+	#spans = ['day', 'week', 'month', 'year']
+	spans = ['year']
+	for span in spans:
+		plot_mult_in_one(entries, span)
+
+
+if __name__ == '__main__':
+
+	test_sqlite_query()
+	entries = db_entries()
+	test_plot_collection(entries)
